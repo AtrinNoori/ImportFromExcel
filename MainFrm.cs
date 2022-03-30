228f97134d3fba3bb441500ec6b8c2ec50f1d08c
AtrinNoori <atrin_xbox@yahoo.com>
2022-03-30 16:43:45 +0430

    Add project files.

---
+++ MainFrm.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace ImportFromExcel
+{
+    public partial class MainFrm : Form
+    {
+        public MainFrm()
+        {
+            InitializeComponent();
+        }
+
+        private void CloseBtn_PICBX_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Application.Exit();
+        }
+
+        private void MainFrm_Load(object sender, EventArgs e)
+        {
+            this.CenterToScreen();
+            Import_BTN.Enabled = false;
+            Export_BTN.Enabled = false;
+            CloseBtn_PICBX.Image = Properties.Resources.Close_20;
+            VersionInfo_LBL.Text = "Version: "+ Application.ProductVersion;
+
+        }
+
+        private void Import_BTN_Click(object sender, EventArgs e)
+        {
+
+            if (FileName_LBL.Text != "")
+            {
+                // Clears datagrideview before each import
+                Data_GRD.Rows.Clear();
+
+
+                // Creating variables to open Excel files
+                Microsoft.Office.Interop.Excel.Application xlApp;
+                Microsoft.Office.Interop.Excel.Workbook xlWorkbook;
+                Microsoft.Office.Interop.Excel.Worksheet xlWorksheet;
+                Microsoft.Office.Interop.Excel.Range xlRange;
+
+                try
+                {
+                    // Assigning file attirbutes to its coresponding variable
+                    xlApp = new Microsoft.Office.Interop.Excel.Application();
+                    xlWorkbook = xlApp.Workbooks.Open(FileName_LBL.Text);
+                    xlWorksheet = xlWorkbook.Worksheets["Sheet1"];
+                    xlRange = xlWorksheet.UsedRange;
+
+                    // Iteration through the file Rows and Columns 
+                    for (int xlRow = 1; xlRow <= xlRange.Rows.Count; xlRow++)
+                    {
+
+                        // Example : => 12 columns 
+                        Data_GRD.Rows.Add(xlRange.Cells[xlRow, 1].Text, xlRange.Cells[xlRow, 2].Text,
+                            xlRange.Cells[xlRow, 3].Text, xlRange.Cells[xlRow, 4].Text, xlRange.Cells[xlRow, 5].Text,
+                            xlRange.Cells[xlRow, 6].Text, xlRange.Cells[xlRow, 7].Text, xlRange.Cells[xlRow, 8].Text,
+                            xlRange.Cells[xlRow, 9].Text, xlRange.Cells[xlRow, 10].Text, xlRange.Cells[xlRow, 11].Text,
+                            xlRange.Cells[xlRow, 12].Text);
+                    }
+                    // MUST DO THE FOLLOWING TO CLOSE THE EXCEL APPLICATION AND PREVENT BLUESCREEN!
+                    xlWorkbook.Close();
+                    xlApp.Quit();
+
+                    Export_BTN.Enabled = true;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Unexpected error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+
+            }
+        }
+
+        private void FileSelect_BTN_Click(object sender, EventArgs e)
+        {
+
+            // File Dialog to find and select excel files
+            using (OpenFileDialog ofp = new OpenFileDialog())
+            {
+                ofp.RestoreDirectory = false;
+                ofp.Filter = "Excel Files | *.xlsx;*.xls;*.xlm";
+                ofp.Title = "Choose the file to import";
+                if (ofp.ShowDialog() == DialogResult.OK)
+                {
+                    FileName_LBL.Text = ofp.FileName;
+                    Import_BTN.Enabled = true;
+                    info_LBL.Visible = true;
+                }
+            }
+        }
+
+        private void Export_BTN_Click(object sender, EventArgs e)
+        {
+            string exportPath;
+
+            // Creating filename using the selected excel file and extractiing the filename without extention
+            // Example : => File extention will be .examp
+            exportPath = Environment.CurrentDirectory + @"\Export\" + Path.GetFileNameWithoutExtension(FileName_LBL.Text) +".exmp";
+
+
+            // Checking for the directory to store the file
+            if (!Directory.Exists(Environment.CurrentDirectory + @"\Export\"))
+                Directory.CreateDirectory(Environment.CurrentDirectory + @"\Export\");
+
+            try
+            {
+                // Copying datagridview items into an array of type byte[] with the size of (Rows * Column)
+                // Example : => Array size is 144
+                byte[] map_Temparray = new byte[144];
+
+
+                // Procedure to add datagridview items into the array
+                for (int i = 0; i < 12; i++)
+                    for (int j = 0; j < 12; j++)
+                        if (Data_GRD.Rows[i].Cells[j].Value != null)
+                            map_Temparray[i + (j * 12)] = (Byte.Parse(Data_GRD.Rows[i].Cells[j].Value.ToString()));
+
+
+                // Creating and storing the Exported file
+                using (FileStream fstr = new FileStream(exportPath, FileMode.Create, FileAccess.Write))
+                {
+                    BinaryFormatter bf = new BinaryFormatter();
+                    bf.Serialize(fstr, map_Temparray);
+
+                    // MUST CLOSE THE FILESTREAM IN ORDER TO PROPERLY SAVE THE FILE
+                    fstr.Close();
+                }
+                MessageBox.Show("Data Exported", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Unexpected error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
---
+++ MainFrm.designer.cs
@@ -0,0 +1,360 @@
+﻿
+namespace ImportFromExcel
+{
+    partial class MainFrm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TopTitle_PNL = new System.Windows.Forms.Panel();
+            this.CloseBtn_PICBX = new System.Windows.Forms.PictureBox();
+            this.title_LBL = new System.Windows.Forms.Label();
+            this.Grid_PNL = new System.Windows.Forms.Panel();
+            this.Data_GRD = new System.Windows.Forms.DataGridView();
+            this.FileSelect_BTN = new System.Windows.Forms.Button();
+            this.Toolbar_PNL = new System.Windows.Forms.Panel();
+            this.FileName_LBL = new System.Windows.Forms.Label();
+            this.info_LBL = new System.Windows.Forms.Label();
+            this.Import_BTN = new System.Windows.Forms.Button();
+            this.Export_BTN = new System.Windows.Forms.Button();
+            this.VersionInfo_LBL = new System.Windows.Forms.Label();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column9 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column10 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column11 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column12 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TopTitle_PNL.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.CloseBtn_PICBX)).BeginInit();
+            this.Grid_PNL.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Data_GRD)).BeginInit();
+            this.Toolbar_PNL.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // TopTitle_PNL
+            // 
+            this.TopTitle_PNL.BackColor = System.Drawing.Color.DodgerBlue;
+            this.TopTitle_PNL.Controls.Add(this.VersionInfo_LBL);
+            this.TopTitle_PNL.Controls.Add(this.CloseBtn_PICBX);
+            this.TopTitle_PNL.Controls.Add(this.title_LBL);
+            this.TopTitle_PNL.Dock = System.Windows.Forms.DockStyle.Top;
+            this.TopTitle_PNL.ForeColor = System.Drawing.Color.Sienna;
+            this.TopTitle_PNL.Location = new System.Drawing.Point(0, 0);
+            this.TopTitle_PNL.Name = "TopTitle_PNL";
+            this.TopTitle_PNL.Size = new System.Drawing.Size(762, 28);
+            this.TopTitle_PNL.TabIndex = 0;
+            // 
+            // CloseBtn_PICBX
+            // 
+            this.CloseBtn_PICBX.Dock = System.Windows.Forms.DockStyle.Right;
+            this.CloseBtn_PICBX.Location = new System.Drawing.Point(727, 0);
+            this.CloseBtn_PICBX.Name = "CloseBtn_PICBX";
+            this.CloseBtn_PICBX.Size = new System.Drawing.Size(35, 28);
+            this.CloseBtn_PICBX.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.CloseBtn_PICBX.TabIndex = 1;
+            this.CloseBtn_PICBX.TabStop = false;
+            this.CloseBtn_PICBX.Click += new System.EventHandler(this.CloseBtn_PICBX_Click);
+            // 
+            // title_LBL
+            // 
+            this.title_LBL.AutoSize = true;
+            this.title_LBL.Dock = System.Windows.Forms.DockStyle.Left;
+            this.title_LBL.Font = new System.Drawing.Font("Bebas", 12F);
+            this.title_LBL.ForeColor = System.Drawing.SystemColors.Control;
+            this.title_LBL.Location = new System.Drawing.Point(0, 0);
+            this.title_LBL.Name = "title_LBL";
+            this.title_LBL.Size = new System.Drawing.Size(145, 21);
+            this.title_LBL.TabIndex = 0;
+            this.title_LBL.Text = "Import     From      Excel";
+            this.title_LBL.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // Grid_PNL
+            // 
+            this.Grid_PNL.Controls.Add(this.Data_GRD);
+            this.Grid_PNL.Dock = System.Windows.Forms.DockStyle.Top;
+            this.Grid_PNL.Location = new System.Drawing.Point(0, 28);
+            this.Grid_PNL.Name = "Grid_PNL";
+            this.Grid_PNL.Size = new System.Drawing.Size(762, 318);
+            this.Grid_PNL.TabIndex = 1;
+            // 
+            // Data_GRD
+            // 
+            this.Data_GRD.AllowUserToAddRows = false;
+            this.Data_GRD.AllowUserToDeleteRows = false;
+            this.Data_GRD.AllowUserToResizeColumns = false;
+            this.Data_GRD.AllowUserToResizeRows = false;
+            this.Data_GRD.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Data_GRD.ClipboardCopyMode = System.Windows.Forms.DataGridViewClipboardCopyMode.Disable;
+            this.Data_GRD.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Data_GRD.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6,
+            this.Column7,
+            this.Column8,
+            this.Column9,
+            this.Column10,
+            this.Column11,
+            this.Column12});
+            this.Data_GRD.Location = new System.Drawing.Point(11, 11);
+            this.Data_GRD.Name = "Data_GRD";
+            this.Data_GRD.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.Data_GRD.RowTemplate.Height = 30;
+            this.Data_GRD.RowTemplate.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.Data_GRD.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
+            this.Data_GRD.Size = new System.Drawing.Size(741, 297);
+            this.Data_GRD.TabIndex = 0;
+            // 
+            // FileSelect_BTN
+            // 
+            this.FileSelect_BTN.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.FileSelect_BTN.Location = new System.Drawing.Point(13, 354);
+            this.FileSelect_BTN.Name = "FileSelect_BTN";
+            this.FileSelect_BTN.Size = new System.Drawing.Size(113, 38);
+            this.FileSelect_BTN.TabIndex = 2;
+            this.FileSelect_BTN.Text = "Select File";
+            this.FileSelect_BTN.UseVisualStyleBackColor = true;
+            this.FileSelect_BTN.Click += new System.EventHandler(this.FileSelect_BTN_Click);
+            // 
+            // Toolbar_PNL
+            // 
+            this.Toolbar_PNL.BackColor = System.Drawing.Color.Orchid;
+            this.Toolbar_PNL.Controls.Add(this.FileName_LBL);
+            this.Toolbar_PNL.Controls.Add(this.info_LBL);
+            this.Toolbar_PNL.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.Toolbar_PNL.Location = new System.Drawing.Point(0, 406);
+            this.Toolbar_PNL.Name = "Toolbar_PNL";
+            this.Toolbar_PNL.Size = new System.Drawing.Size(762, 25);
+            this.Toolbar_PNL.TabIndex = 3;
+            // 
+            // FileName_LBL
+            // 
+            this.FileName_LBL.Dock = System.Windows.Forms.DockStyle.Left;
+            this.FileName_LBL.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.FileName_LBL.ForeColor = System.Drawing.SystemColors.Control;
+            this.FileName_LBL.Location = new System.Drawing.Point(106, 0);
+            this.FileName_LBL.Name = "FileName_LBL";
+            this.FileName_LBL.Size = new System.Drawing.Size(653, 25);
+            this.FileName_LBL.TabIndex = 2;
+            this.FileName_LBL.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // info_LBL
+            // 
+            this.info_LBL.AutoSize = true;
+            this.info_LBL.Dock = System.Windows.Forms.DockStyle.Left;
+            this.info_LBL.Font = new System.Drawing.Font("Bebas", 12F);
+            this.info_LBL.ForeColor = System.Drawing.SystemColors.Control;
+            this.info_LBL.Location = new System.Drawing.Point(0, 0);
+            this.info_LBL.Name = "info_LBL";
+            this.info_LBL.Size = new System.Drawing.Size(106, 21);
+            this.info_LBL.TabIndex = 1;
+            this.info_LBL.Text = "selected     file";
+            this.info_LBL.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.info_LBL.Visible = false;
+            // 
+            // Import_BTN
+            // 
+            this.Import_BTN.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.Import_BTN.Location = new System.Drawing.Point(131, 354);
+            this.Import_BTN.Name = "Import_BTN";
+            this.Import_BTN.Size = new System.Drawing.Size(113, 38);
+            this.Import_BTN.TabIndex = 4;
+            this.Import_BTN.Text = "Import";
+            this.Import_BTN.UseVisualStyleBackColor = true;
+            this.Import_BTN.Click += new System.EventHandler(this.Import_BTN_Click);
+            // 
+            // Export_BTN
+            // 
+            this.Export_BTN.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.Export_BTN.Location = new System.Drawing.Point(640, 355);
+            this.Export_BTN.Name = "Export_BTN";
+            this.Export_BTN.Size = new System.Drawing.Size(113, 38);
+            this.Export_BTN.TabIndex = 5;
+            this.Export_BTN.Text = "Export To File";
+            this.Export_BTN.UseVisualStyleBackColor = true;
+            this.Export_BTN.Click += new System.EventHandler(this.Export_BTN_Click);
+            // 
+            // VersionInfo_LBL
+            // 
+            this.VersionInfo_LBL.AutoSize = true;
+            this.VersionInfo_LBL.Dock = System.Windows.Forms.DockStyle.Left;
+            this.VersionInfo_LBL.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.VersionInfo_LBL.ForeColor = System.Drawing.SystemColors.Control;
+            this.VersionInfo_LBL.Location = new System.Drawing.Point(145, 0);
+            this.VersionInfo_LBL.Name = "VersionInfo_LBL";
+            this.VersionInfo_LBL.Size = new System.Drawing.Size(48, 15);
+            this.VersionInfo_LBL.TabIndex = 2;
+            this.VersionInfo_LBL.Text = "Version:";
+            this.VersionInfo_LBL.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            // 
+            // Column1
+            // 
+            this.Column1.Frozen = true;
+            this.Column1.HeaderText = "1";
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 50;
+            // 
+            // Column2
+            // 
+            this.Column2.Frozen = true;
+            this.Column2.HeaderText = "2";
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 50;
+            // 
+            // Column3
+            // 
+            this.Column3.Frozen = true;
+            this.Column3.HeaderText = "3";
+            this.Column3.Name = "Column3";
+            this.Column3.Width = 50;
+            // 
+            // Column4
+            // 
+            this.Column4.Frozen = true;
+            this.Column4.HeaderText = "4";
+            this.Column4.Name = "Column4";
+            this.Column4.Width = 50;
+            // 
+            // Column5
+            // 
+            this.Column5.Frozen = true;
+            this.Column5.HeaderText = "5";
+            this.Column5.Name = "Column5";
+            this.Column5.Width = 50;
+            // 
+            // Column6
+            // 
+            this.Column6.Frozen = true;
+            this.Column6.HeaderText = "6";
+            this.Column6.Name = "Column6";
+            this.Column6.Width = 50;
+            // 
+            // Column7
+            // 
+            this.Column7.Frozen = true;
+            this.Column7.HeaderText = "7";
+            this.Column7.Name = "Column7";
+            this.Column7.Width = 50;
+            // 
+            // Column8
+            // 
+            this.Column8.Frozen = true;
+            this.Column8.HeaderText = "8";
+            this.Column8.Name = "Column8";
+            this.Column8.Width = 50;
+            // 
+            // Column9
+            // 
+            this.Column9.Frozen = true;
+            this.Column9.HeaderText = "9";
+            this.Column9.Name = "Column9";
+            this.Column9.Width = 50;
+            // 
+            // Column10
+            // 
+            this.Column10.Frozen = true;
+            this.Column10.HeaderText = "10";
+            this.Column10.Name = "Column10";
+            this.Column10.Width = 50;
+            // 
+            // Column11
+            // 
+            this.Column11.Frozen = true;
+            this.Column11.HeaderText = "11";
+            this.Column11.Name = "Column11";
+            this.Column11.Width = 50;
+            // 
+            // Column12
+            // 
+            this.Column12.Frozen = true;
+            this.Column12.HeaderText = "12";
+            this.Column12.Name = "Column12";
+            this.Column12.Width = 50;
+            // 
+            // MainFrm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(762, 431);
+            this.Controls.Add(this.Export_BTN);
+            this.Controls.Add(this.Import_BTN);
+            this.Controls.Add(this.Toolbar_PNL);
+            this.Controls.Add(this.FileSelect_BTN);
+            this.Controls.Add(this.Grid_PNL);
+            this.Controls.Add(this.TopTitle_PNL);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "MainFrm";
+            this.Text = "Import From Excel";
+            this.Load += new System.EventHandler(this.MainFrm_Load);
+            this.TopTitle_PNL.ResumeLayout(false);
+            this.TopTitle_PNL.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.CloseBtn_PICBX)).EndInit();
+            this.Grid_PNL.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.Data_GRD)).EndInit();
+            this.Toolbar_PNL.ResumeLayout(false);
+            this.Toolbar_PNL.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel TopTitle_PNL;
+        private System.Windows.Forms.PictureBox CloseBtn_PICBX;
+        private System.Windows.Forms.Label title_LBL;
+        private System.Windows.Forms.Panel Grid_PNL;
+        private System.Windows.Forms.DataGridView Data_GRD;
+        private System.Windows.Forms.Button FileSelect_BTN;
+        private System.Windows.Forms.Panel Toolbar_PNL;
+        private System.Windows.Forms.Label FileName_LBL;
+        private System.Windows.Forms.Label info_LBL;
+        private System.Windows.Forms.Button Import_BTN;
+        private System.Windows.Forms.Button Export_BTN;
+        private System.Windows.Forms.Label VersionInfo_LBL;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column8;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column9;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column10;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column11;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column12;
+    }
+}
+
